39938dcacab88e2feedcaed2f25220d600258b62
Nxky83 <129763676+Nxky83@users.noreply.github.com>
2025-03-09 10:31:04 +0100

    patch porte

--- PulseOfFear (3)/Assets/Scripts/Animation/Hall/DoorHall.cs
+++ PulseOfFear (3)/Assets/Scripts/Animation/Hall/DoorHall.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class DoorHall1 : MonoBehaviour
 {
     private Animator animator;
-    private bool isOpen = false;
-    private bool isAnimating = false;
+    private bool isOpen = false; // Indique si la porte est ouverte ou fermée
+    private bool isAnimating = false; // Vérifie si une animation est en cours
 
     void Start()
     {
@@ -14,6 +14,7 @@ public class DoorHall1 : MonoBehaviour
 
     public void ToggleDoorHall()
     {
+        // Ne déclenche l'animation que si aucune animation n'est en cours
         if (!isAnimating)
         {
             StartCoroutine(PlayAnimation());
@@ -22,23 +23,24 @@ public class DoorHall1 : MonoBehaviour
 
     private IEnumerator PlayAnimation()
     {
-        isAnimating = true;
+        isAnimating = true; // Empêche d'autres animations de démarrer
 
+        // Si la porte est ouverte, on joue l'animation de fermeture, sinon l'animation d'ouverture
         if (isOpen)
         {
-            animator.Play("doorhallopen");
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Envt/Enviro_action_interract_porte_open", GetComponent<Transform>().position);
+            animator.Play("doorhallclose");
         }
         else
         {
-            animator.Play("doorhallclose");
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Envt/Enviro_action_interract_porte_close", GetComponent<Transform>().position);
+            animator.Play("doorhallopen");
         }
 
-        isOpen = !isOpen;
-
+        // Attend la fin de l'animation avant de changer l'état de la porte
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
 
-        isAnimating = false;
+        // Inverse l'état de la porte seulement après l'animation
+        isOpen = !isOpen;
+
+        isAnimating = false; // Permet de rejouer l'animation si nécessaire
     }
 }
--- PulseOfFear (3)/Assets/Scripts/Animation/Manequin/DoorHall3.cs
+++ PulseOfFear (3)/Assets/Scripts/Animation/Manequin/DoorHall3.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class DoorHall3 : MonoBehaviour
 {
     private Animator animator;
-    private bool isOpen = false;
-    private bool isAnimating = false;
+    private bool isOpen = false; // Indique si la porte est ouverte ou fermée
+    private bool isAnimating = false; // Vérifie si une animation est en cours
 
     void Start()
     {
@@ -14,6 +14,7 @@ public class DoorHall3 : MonoBehaviour
 
     public void ToggleDoorHall3()
     {
+        // Ne déclenche l'animation que si aucune animation n'est en cours
         if (!isAnimating)
         {
             StartCoroutine(PlayAnimation());
@@ -22,23 +23,26 @@ public class DoorHall3 : MonoBehaviour
 
     private IEnumerator PlayAnimation()
     {
-        isAnimating = true;
+        isAnimating = true; // Empêche d'autres animations de démarrer
 
+        // Si la porte est ouverte, on joue l'animation de fermeture, sinon l'animation d'ouverture
         if (isOpen)
-        {
-            animator.Play("doormanequinopen");
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Envt/Enviro_action_interract_porte_open", GetComponent<Transform>().position);
-        }
-        else
         {
             animator.Play("doormanequinclose");
             FMODUnity.RuntimeManager.PlayOneShot("event:/Envt/Enviro_action_interract_porte_close", GetComponent<Transform>().position);
         }
+        else
+        {
+            animator.Play("doormanequinopen");
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Envt/Enviro_action_interract_porte_open", GetComponent<Transform>().position);
+        }
 
-        isOpen = !isOpen;
-
+        // Attend la fin de l'animation avant de changer l'état de la porte
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
 
-        isAnimating = false;
+        // Inverse l'état de la porte seulement après l'animation
+        isOpen = !isOpen;
+
+        isAnimating = false; // Permet de rejouer l'animation si nécessaire
     }
 }
